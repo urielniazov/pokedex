2ee699aa43e179b96a56fe7e09f65f519c337c5c
Uriel Niazov <urielniazov@gmail.com>
2025-03-25 10:18:19 +0200

    fixed issue

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ CORS(app)
 
 app.register_blueprint(pokemon_bp)
 
-@app.route('/api/icon')
-def get_icon_url():
-    name = request.args.get('name', default=None, type=str)
-    generation = request.args.get('generation', default=1, type=int)
-    if generation < 5:
-        return f"https://img.pokemondb.net/sprites/silver/normal/{name}.png"
-    return f"https://img.pokemondb.net/sprites/x-y/normal/{name}.png"
-
 @app.route('/')
 def hello():
     return "Pokédex API is running!"
